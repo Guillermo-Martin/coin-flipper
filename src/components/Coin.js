a73f25e19a5db7e74f9c2e27e710ff1e0600bd37
gcmartin <gscalica@gmail.com>
2020-07-17 16:52:37 -0700

    Change alt in img tag to props

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Coin(props) {
   return(
     <div>
-      <img src={props.face} alt="tails"></img>
+      <img src={props.face} alt={props.alt}></img>
     </div>
   );
 }
