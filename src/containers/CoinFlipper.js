b9bef1740eac144fef7cc5fb46727f878f7905a8
gcmartin <gscalica@gmail.com>
2020-07-17 16:42:30 -0700

    Create flipCoin function

--- src/containers/CoinFlipper.js
+++ src/containers/CoinFlipper.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import Coin from './../components/Coin';
 
 class CoinFlipper extends Component {
+  
+  flipCoin = () => {
+    // generate either 1 or 2
+    let randomNum = Math.floor((Math.random() * 2) + 1);
+    // if randomNum is 1, display heads; otherwise, display tails
+    if(randomNum === 1) {
+      console.log("Heads!")
+    } else {
+      console.log("Tails!");
+    }
+  }
+
   render() {
     return (
       <div>
-        <Coin />
+        {/* <Coin /> */}
+        <button onClick={this.flipCoin}>Flip!</button>
       </div>
     );
   }
