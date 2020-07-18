5c2851dbf07ae6ec04275f39d8602315cb23f4b8
gcmartin <gscalica@gmail.com>
2020-07-17 17:05:57 -0700

    Add state, revise props for Coin component, and add img src to flipCoin function

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -10,5 +10,3 @@ function Coin(props) {
 
 export default Coin;
 
-
-// {/* <img src="https://upload.wikimedia.org/wikipedia/commons/c/cd/S_Half_Dollar_Obverse_2016.jpg" alt="heads"></img> */}
--- src/containers/CoinFlipper.js
+++ src/containers/CoinFlipper.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 import Coin from './../components/Coin';
 
 class CoinFlipper extends Component {
-  static defaultProps = {
+  // static defaultProps = {
+  //   face: "http://www.pcgscoinfacts.com/UserImages/71009269r.jpg",
+  //   alt: "tails"
+  // }
+
+  state = {
     face: "http://www.pcgscoinfacts.com/UserImages/71009269r.jpg",
     alt: "tails"
   }
@@ -12,19 +17,19 @@ class CoinFlipper extends Component {
     let randomNum = Math.floor((Math.random() * 2) + 1);
     // if randomNum is 1, display heads; otherwise, display tails
     if(randomNum === 1) {
-      console.log("Heads!")
+      this.setState({ face: "http://www.pcgscoinfacts.com/UserImages/71009269r.jpg", alt: "tails" });
     } else {
-      console.log("Tails!");
+      this.setState({ face: "https://upload.wikimedia.org/wikipedia/commons/c/cd/S_Half_Dollar_Obverse_2016.jpg", alt: "heads"});
     }
   }
 
   render() {
     return (
       <div>
-        {/* pass in props to coin; props will be the source of the image */}
+        {/* pass in state as props to coin */}
         <Coin 
-          face={this.props.face} 
-          alt={this.props.alt}
+          face={this.state.face} 
+          alt={this.state.alt}
         />
 
         {/* button */}
