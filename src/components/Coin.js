d65265e02b8c46c60233deb95eb29e7da66f10cf
gcmartin <gscalica@gmail.com>
2020-07-17 17:09:12 -0700

    Create 'assets' folder and styles.css, style Coin component

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './../assets/styles.css';
 
 function Coin(props) {
   return(
     <div>
-      <img src={props.face} alt={props.alt}></img>
+      <img src={props.face} alt={props.alt} className="Coin"></img>
     </div>
   );
 }
