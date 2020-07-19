ea697f131f57b3ffe02fd8f1b9547269451fe5af
gcmartin <gscalica@gmail.com>
2020-07-18 18:08:17 -0700

    Add state for flips, tails, and heads and setState to if statement

--- src/containers/CoinFlipper.js
+++ src/containers/CoinFlipper.js
@@ -9,17 +9,24 @@ class CoinFlipper extends Component {
 
   state = {
     face: "http://www.pcgscoinfacts.com/UserImages/71009269r.jpg",
-    alt: "tails"
+    alt: "tails",
+    flips: 0,
+    tails: 0,
+    heads: 0
   }
 
   flipCoin = () => {
     // generate either 1 or 2
     let randomNum = Math.floor((Math.random() * 2) + 1);
+    // add 1 to the 'flips' count
+    this.setState(curState => ({ flips: curState.flips + 1}));
     // if randomNum is 1, display heads; otherwise, display tails
     if(randomNum === 1) {
       this.setState({ face: "http://www.pcgscoinfacts.com/UserImages/71009269r.jpg", alt: "tails" });
+      this.setState(curState => ({ tails: curState.tails + 1}));
     } else {
       this.setState({ face: "https://upload.wikimedia.org/wikipedia/commons/c/cd/S_Half_Dollar_Obverse_2016.jpg", alt: "heads"});
+      this.setState(curState => ({ heads: curState.heads + 1}));
     }
   }
 
@@ -32,6 +39,7 @@ class CoinFlipper extends Component {
           alt={this.state.alt}
         />
 
+        <h2>Out of {this.state.flips} flips, there have been {this.state.heads} heads and {this.state.tails} tails.</h2>
         {/* button */}
         <button onClick={this.flipCoin}>Flip!</button>
       </div>
